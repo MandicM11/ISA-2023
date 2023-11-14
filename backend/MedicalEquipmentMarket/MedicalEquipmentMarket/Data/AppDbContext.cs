0baa2f88dabdc6bd1f37370e0cf19a7c126e7d1f
darkofilipovic <filipovic.ra67.2019@uns.ac.rs>
2023-11-14 15:40:14 +0100

    postgre database connection established

---
+++ backend/MedicalEquipmentMarket/MedicalEquipmentMarket/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace MedicalEquipmentMarket.Data
+{
+    public class AppDbContext:DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+    }
+}
