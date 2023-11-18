6c2aac3c4e41241a850f23feb6946f6328e0da87
darkofilipovic <filipovic.ra67.2019@uns.ac.rs>
2023-11-18 18:26:54 +0100

    company and reservations added

---
+++ backend/MedicalEquipmentMarket/MedicalEquipmentMarket/Model/Company.cs
@@ -0,0 +1,24 @@
+﻿namespace MedicalEquipmentMarket.Model
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address{ get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Company()
+        {
+
+        }
+        public Company(int id, string name, string address, string city, string phoneNumber)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            City = city;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
---
+++ backend/MedicalEquipmentMarket/MedicalEquipmentMarket/Model/Reservation.cs
@@ -0,0 +1,24 @@
+﻿namespace MedicalEquipmentMarket.Model
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int BuyerAccountId { get; set; }
+        public int CompanyId { get; set; }
+        public int EquipmentId { get; set; }
+        public DateTime ReservationTime { get; set; }
+
+        public Reservation()
+        {
+
+        }
+        public Reservation(int id, int buyerAccountId, int companyId, int equipmentId, DateTime reservationTime)
+        {
+            Id = id;
+            BuyerAccountId = buyerAccountId;
+            CompanyId = companyId;
+            EquipmentId = equipmentId;
+            ReservationTime = reservationTime;
+        }
+    }
+}
