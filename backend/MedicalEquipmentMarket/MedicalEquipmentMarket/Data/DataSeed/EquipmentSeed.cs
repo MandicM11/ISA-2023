6e8dba728cdc5febcecafc15e74b9ca1f92e7afe
darkofilipovic <filipovic.ra67.2019@uns.ac.rs>
2023-11-18 17:40:32 +0100

    Equipment model and seeding data implemented

---
+++ backend/MedicalEquipmentMarket/MedicalEquipmentMarket/Data/DataSeed/EquipmentSeed.cs
@@ -0,0 +1,19 @@
+﻿using MedicalEquipmentMarket.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalEquipmentMarket.Data.DataSeed
+{
+    public static class EquipmentSeed
+    {
+        public static void SeedEquipment(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Equipment>(f =>
+            {
+            f.HasData(new Equipment(1, "Stetoskop", "Aparat za osluskivanja rada pluca i srca", 20000),
+                      new Equipment(2, "Krevet", "Namestaj za pacijente", 14000),
+                      new Equipment(3, "Pakovanje zavoja", "Oprema za previjanje", 1200)
+                );
+            });
+        }
+    }
+}
