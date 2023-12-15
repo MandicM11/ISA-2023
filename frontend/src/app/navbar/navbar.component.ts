483e9e024396584b7424be418118c07aeeb817d1
darkofilipovic <filipovic.ra67.2019@uns.ac.rs>
2023-12-15 22:31:52 +0100

    routing implemented,added registration page

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
-  
+  constructor(
+    private router: Router) {}
+  register(): void {
+    this.router.navigate(['/register']);
+  }
 }
