d55f2d0de5b350c3c179a974ef024d73a3339cdf
darkofilipovic <filipovic.ra67.2019@uns.ac.rs>
2023-12-17 20:51:24 +0100

    login and registration fully implemented

--- backend/MedicalEquipmentMarket/MedicalEquipmentMarket/Services/IAuthService.cs
+++ backend/MedicalEquipmentMarket/MedicalEquipmentMarket/Services/IAuthService.cs
@@ -1,10 +1,12 @@
 ﻿using MedicalEquipmentMarket.Security.Model;
+using Microsoft.AspNetCore.Identity;
 
 namespace MedicalEquipmentMarket.Services
 {
     public interface IAuthService
     {
-        Task<bool> RegisterUser(LoginRequest request);
+        Task<bool> RegisterUser(RegisterRequest request);
         Task<bool> Login(LoginRequest request);
+        public int GetUserId(string username);
     }
 }
